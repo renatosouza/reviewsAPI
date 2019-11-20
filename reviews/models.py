10906fee5f572ff4219eec8ae72726f749a829a6
Renato <renatosouzavv@gmail.com>
2019-11-20 15:32:52 -0300

    Created review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
+RATING_CHOICES = [
+    (1, 'not satisfied'),
+    (2, 'partly satisfied'),
+    (3, 'satisfied'),
+    (4, 'more than satisfied'),
+    (5, 'very satisfied')
+]
+
+
 # Create your models here.
+class Review(models.Model):
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    title = models.CharField(max_length=64)
+    summary = models.TextField()
+    ip_address = models.CharField(max_length=15)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    company = models.CharField(max_length=50)
+    reviewer = models.CharField(max_length=50)
+    
+    class Meta:
+        ordering = ['-submitted_at']
