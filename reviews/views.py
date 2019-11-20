b79b2e4f15209a3b2fa9e5159c483e29bfee9f96
Renato <renatosouzavv@gmail.com>
2019-11-20 15:45:34 -0300

    Created review views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Review
+from .serializers import ReviewSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
