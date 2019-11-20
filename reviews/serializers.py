ea592d3cbed0cc0f5a842c8de653795609322c06
Renato <renatosouzavv@gmail.com>
2019-11-20 15:52:15 -0300

    Minor fix to show date of submission

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,8 +3,9 @@ from .models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    submitted_at = serializers.DateTimeField(read_only=True)
     
     class Meta:
         model = Review
         fields = ['id', 'rating', 'title', 'summary', 
-                  'ip_address', 'company', 'reviewer']
+                  'ip_address', 'company', 'reviewer', 'submitted_at']
