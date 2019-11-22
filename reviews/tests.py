3c37a4bf1a447790e246c9c63fd3956614f4ad86
Renato <renatosouzavv@gmail.com>
2019-11-22 18:30:14 -0300

    Reorganized tests for models

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,7 +1,7 @@
 import unittest
 from django.test import TestCase
 from .models import Review
-from .serializers import ReviewSerializer, UserSerializer
+from .serializers import ReviewSerializer
 from django.contrib.auth.models import User
 
 
@@ -10,45 +10,68 @@ class ReviewTest(TestCase):
     """ Testing model Review """
     
     def setUp(self):
-        user = User.objects.create_user(username="testy", password="1234")
-        Review.objects.create(rating=1, 
-                              title="Testing1", 
-                              summary="this is a test", 
-                              ip_address="1.1.1.1", 
-                              company="Tester", 
-                              reviewer="Testy Tester",
-                              owner=user)
-        Review.objects.create(rating=2, 
-                              title="Testing2", 
-                              summary="this is a new test", 
-                              ip_address="1.1.1.1", 
-                              company="Tester", 
-                              reviewer="Testy Tester",
-                              owner=user)
+        self.user = User.objects.create_user(username="tester", 
+                                             password="1234")
+        self.review1_attributes = {
+            'rating': 1,
+            'title': 'Awful book',
+            'summary': 'Worst book I have ever read',
+            'ip_address': '1.1.1.1',
+            'company': 'Library Books',
+            'reviewer': 'Book Reader',
+            'owner': self.user
+        }
+        self.review2_attributes = {
+            'rating': 3,
+            'title': 'Regular sushi',
+            'summary': 'Not the worst, but it could be better',
+            'ip_address': '2.2.2.2',
+            'company': 'Sushi Place',
+            'reviewer': 'Sushi Eater',
+            'owner': self.user
+        }
+        self.review1 = Review.objects.create(**self.review1_attributes)
+        self.review2 = Review.objects.create(**self.review2_attributes)
         
+    def test_rating_content(self):
+        self.assertEqual(self.review1_attributes['rating'], 
+                         self.review1.rating)
         
-    def test_fields(self):
-        review_test = Review.objects.get(title="Testing1")
-        self.assertEqual(review_test.company, "Tester")
-        self.assertEqual(review_test.summary, "this is a test")
-        self.assertEqual(review_test.reviewer, "Testy Tester")
-        self.assertEqual(review_test.ip_address, "1.1.1.1")
+    def test_title_content(self):
+        self.assertEqual(self.review1_attributes['title'], 
+                         self.review1.title)
+    
+    def test_summary_content(self):
+        self.assertEqual(self.review1_attributes['summary'], 
+                         self.review1.summary)
+        
+    def test_ip_address_content(self):
+        self.assertEqual(self.review1_attributes['ip_address'], 
+                         self.review1.ip_address)
+    
+    def test_company_content(self):
+        self.assertEqual(self.review1_attributes['company'], 
+                         self.review1.company)
+        
+    def test_reviewer_content(self):
+        self.assertEqual(self.review1_attributes['reviewer'], 
+                         self.review1.reviewer)
+        
+    def test_owner_content(self):
+        self.assertEqual(self.review1_attributes['owner'], 
+                         self.review1.owner)    
         
     def test_order_of_reviews(self):
         #the first created is the last in the list
-        review_test = Review.objects.get(title="Testing1")
         last_review = Review.objects.last()
-        self.assertEqual(review_test, last_review)
+        self.assertEqual(self.review1, last_review)
         
         
 class ReviewSerializerTeste(TestCase):
     """ Testing ReviewSerializer """
     
     def setUp(self):
-        try:
-            self.user = User.objects.get(username="tester")
-        except:
-            self.user = User.objects.create_user(username="tester", 
+        self.user = User.objects.create_user(username="tester", 
                                                  password="1234")
         
         self.review_attributes = {
